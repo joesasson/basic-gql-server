6b3a2de4c00ccc6037a3a75c79824c817cc50a2f
joesass <dibson2013@gmail.com>
2018-10-10 20:33:34 -0400

    Rough Draft of schema

--- types.js
+++ types.js
@@ -2,13 +2,32 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
   type Location {
-    _id: String
+    _id: String!
     name: String!
-    address:  String
-    latitude: Float
-    longitude: Float
-    createdAt: Int
-    updatedAt: Int
+    address:  String!
+    latitude: Float!
+    longitude: Float!
+    createdAt: Int!
+    updatedAt: Int!,
+    organization: Organization!
+  }
+
+  type Organization {
+    name: String!,
+    createdAt: Int!,
+    updatedAt: Int!,
+    locations: [Location],
+    events: [Event]
+  }
+
+  type Event {
+    name: String!,
+    date: Int!,
+    time: Int!,
+    description: String!,
+    createdAt: Int!,
+    updatedAt: Int!,
+    organization: Organization!
   }
 
   type Query {
